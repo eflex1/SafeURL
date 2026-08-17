b2527531f16410d11f6cf717dfabe14a154725fd
eflex1 <ecadeniyi@gmail.com>
2026-08-17 02:42:02 +0100

    Fix Aiven DB connection

--- safeurl-backend/server.js
+++ safeurl-backend/server.js
@@ -8,11 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 // MySQL Database Connection Pool
+// Upgraded MySQL Database Connection Pool for Aiven Cloud
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 27693, 
+    ssl: {
+        rejectUnauthorized: true 
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
